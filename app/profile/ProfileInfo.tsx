2c15651fd99033a3db2665a813e4c76889bdba75
Hania Said <haniasaid110@gmail.com>
2025-09-16 13:17:46 +0300

    updated sidebar, page.tsx for profile page, and commented some lines temporarily in profileinfo component to resolve tree hydration issue caused by <input> elements

--- app/profile/ProfileInfo.tsx
+++ app/profile/ProfileInfo.tsx
@@ -10,7 +10,7 @@ export default function ProfileInfo(){
                     <div className="p-4 m-3 rounded-2xl">
                         <h2 className="py-4">Personal Information</h2>
                         <div className="grid gap-6 mb-6 md:grid-cols-2">
-                            <div>
+                            {/* <div>
                                 <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
                                 <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
                             </div>
@@ -21,7 +21,7 @@ export default function ProfileInfo(){
                             <div>
                                 <label htmlFor="birthdate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Birthdate</label>
                                 <input type="date" id="birthdate" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
-                            </div>
+                            </div> */}
                             <div>
                             <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
                             <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -35,7 +35,7 @@ export default function ProfileInfo(){
                     </div>
                     <div className="p-4 m-3 rounded-2xl">
                         <h2 className="py-4">Contact Information</h2>
-                        <div className="grid gap-6 mb-6 md:grid-cols-2">
+                        {/* <div className="grid gap-6 mb-6 md:grid-cols-2">
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Address</label>
                                 <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
@@ -44,7 +44,7 @@ export default function ProfileInfo(){
                                 <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone number</label>
                                 <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
                 <button type="button" className="text-white bg-gray-400 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-2xl text-lg px-5 py-4 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Update Information</button>
