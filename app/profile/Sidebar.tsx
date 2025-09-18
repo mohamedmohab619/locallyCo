30ac5253e0761058e800264c43eedd3db28648bc
Hania Said <haniasaid110@gmail.com>
2025-09-18 06:58:15 +0300

    created initial dashboard

--- app/profile/Sidebar.tsx
+++ app/profile/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { initFlowbite } from 'flowbite';
+// import { initFlowbite } from 'flowbite';
 
 type SidebarProps = {
   title: string;
@@ -9,10 +9,10 @@ type SidebarProps = {
 };
 
 export default function Sidebar(props: SidebarProps){
-   React.useEffect(() => {
-    // Initialize Flowbite's JavaScript
-    initFlowbite();
-  }, []);
+//    React.useEffect(() => {
+//     // Initialize Flowbite's JavaScript
+//     initFlowbite();
+//   }, []);
     return(
          <li className="bg-yellow-50 dark:bg-gray-800">
             <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
