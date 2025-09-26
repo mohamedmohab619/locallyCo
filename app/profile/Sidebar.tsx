4bd64074309b9618aad0b99dc706634f636af07e
MohamedNawsany <dnawasany@gmail.com>
2025-09-26 17:18:37 +0300

    My profile

--- app/profile/Sidebar.tsx
+++ app/profile/Sidebar.tsx
@@ -1,24 +1,64 @@
 "use client";
 
-import React from "react";
-import { initFlowbite } from 'flowbite';
+import { useEffect } from "react";
+import { initFlowbite } from "flowbite";
+import Link from "next/link";
 
-type SidebarProps = {
-  title: string;
-  icon?: React.ReactNode;
-};
-
-export default function Sidebar(props: SidebarProps){
-   React.useEffect(() => {
-    // Initialize Flowbite's JavaScript
+export default function Sidebar({ title }: { title: string }) {
+  useEffect(() => {
     initFlowbite();
   }, []);
-    return(
-         <li className="bg-yellow-50 dark:bg-gray-800">
-            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               {props.icon}
-               <span className="ms-3">{props.title}</span>
-            </a>
-         </li>
-    )
+
+  return (
+    <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+      {/* Title */}
+      <div className="p-4 text-xl font-bold text-gray-900 dark:text-white">
+        {title}
+      </div>
+
+      {/* Sidebar navigation */}
+      <ul className="space-y-2 font-medium">
+        <li>
+          <Link
+            href="/"
+            className="flex items-center p-2 text-gray-700 rounded-lg 
+                       hover:bg-gray-100 dark:text-gray-200 
+                       dark:hover:bg-gray-700"
+          >
+            <span className="ml-3">Home</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/orders"
+            className="flex items-center p-2 text-gray-700 rounded-lg 
+                       hover:bg-gray-100 dark:text-gray-200 
+                       dark:hover:bg-gray-700"
+          >
+            <span className="ml-3">Orders</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/products"
+            className="flex items-center p-2 text-gray-700 rounded-lg 
+                       hover:bg-gray-100 dark:text-gray-200 
+                       dark:hover:bg-gray-700"
+          >
+            <span className="ml-3">Products</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/settings"
+            className="flex items-center p-2 text-gray-700 rounded-lg 
+                       hover:bg-gray-100 dark:text-gray-200 
+                       dark:hover:bg-gray-700"
+          >
+            <span className="ml-3">Settings</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
