b3345e7f71f9315640d10606fe7571263193b7ac
Hania Said <haniasaid110@gmail.com>
2025-09-26 18:55:27 +0300

    Updated filter: grid structure

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,16 +3,58 @@ import Navbar from "../components/NavBar";
 import Filter from "./Filter";
 import Sort from "./Sort";
 import Footer from "../components/Footer";
+import Card from "../favourites/Card";
 
 export default function Page() {
   return (
       <>
          <Navbar />
-         <div className="mt-4 flex flex-row justify-end gap-2 mx-3">
-            <Sort />
-            <Filter />
+         {/* <div className="mt-4 w-full h-full flex flex-row gap-2 mx-3">
+            <div className="justify-self-start">
+               <aside
+                  className="w-64 max-h-[80vh] overflow-y-auto bg-white border border-gray-200 
+                  rounded-2xl">
+                     <Filter />
+               </aside>
+            </div>
+            <div className="h-full flex flex-col">
+
+               <Sort />
+               <Card />
+               <div className="align-self-end">
+
+                  <Footer />
+               </div>
+            </div>
+         </div> */}
+         <div className="grid grid-cols-5 mt-10">
+            <div className="col-span-1 bg-amber-100">
+               <aside
+                  className="md:w-64 max-h-[80vh] overflow-y-auto bg-white border border-gray-200 
+                  rounded-2xl sticky top-20 m-auto">
+                     <Filter />
+               </aside>
+            </div>
+            <div className="col-span-4 bg-green-100">
+               <div>
+                  <Sort />
+               </div>
+               <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 p-3 gap-2 w-full">
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+               </div>
+               
+            </div>
          </div>
-         <Footer />
+         <div>
+            <Footer />
+         </div>
+
       </>
  ); 
 }
