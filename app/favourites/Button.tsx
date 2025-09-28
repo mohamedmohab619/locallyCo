ba384129ee3399118f2b30209fbe96f8c4224fa0
Hania Said <haniasaid110@gmail.com>
2025-09-28 17:17:48 +0300

    Updated header in favourites

--- app/favourites/Button.tsx
+++ app/favourites/Button.tsx
@@ -9,7 +9,7 @@ export default function Button({ onAddCard }: ButtonProps) {
     <button
       type="button"
       onClick={onAddCard}
-      className="m-3 mt-20 text-gray-900 bg-gradient-to-r from-lime-200 via-yellow-100 to-yellow-300 
+      className="m-3 text-gray-900 bg-gradient-to-r from-lime-200 via-yellow-100 to-yellow-300 
                  hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-yellow-50 
                  dark:focus:ring-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 
                  text-center me-2 mb-2 cursor-pointer"
