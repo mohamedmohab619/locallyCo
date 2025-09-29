8addbdb903461f5acc1962f0a01e19e216622750
Hania Said <haniasaid110@gmail.com>
2025-09-29 16:49:19 +0300

    New feature: brand page

---
+++ app/brand/BrandOverview.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import BrandImage from "./assets/Parkville-Brand-Image.webp";
+
+export default function BrandOverview() {
+  return (
+    <section className="grid grid-cols-2 gap-10 mx-20 mt-10">
+      <div className="col-span-1 rounded-2xl">
+        <h2 className="text-2xl mb-10">Brand Name</h2>
+        <p className="text-center px-2">
+          In the vibrant world of skincare and wellness, Parkville stands out as
+          a pioneering Egyptian pharmaceutical firm dedicated to crafting
+          innovative solutions for all your skincare needs. With a deep
+          understanding of the diverse requirements of its customers, Parkville
+          specializes in developing and distributing unique products designed to
+          enhance your beauty and well-being. At the heart of Parkville&apos;s
+          mission is a commitment to quality and effectiveness. The range of
+          Parkville hair products is thoughtfully formulated to cater to various
+          hair types and concerns, ensuring that everyone can find the perfect
+          match for their individual needs.
+        </p>
+      </div>
+      <div className="col-span-1 bg-amber-200 rounded-2xl">
+        <Image src={BrandImage} alt="brand img" className="rounded-2xl" />
+      </div>
+    </section>
+  );
+}
---
+++ app/brand/page.tsx
@@ -0,0 +1,43 @@
+import Navbar from "../components/NavBar";
+import BrandOverview from "./BrandOverview";
+import Footer from "../components/Footer";
+import Filter from "../search/Filter";
+import Sort from "../search/Sort";
+import Card from "../favourites/Card";
+
+export default function Brand() {
+  return (
+    <>
+      <Navbar />
+      <BrandOverview />
+      <section className="mt-10 mx-10">
+        <div className="grid grid-cols-5">
+          <div className="col-span-1">
+            <aside
+              className="md:w-64 mt-15 overflow-y-auto bg-white border border-gray-200 
+                          rounded-2xl sticky top-20 m-auto"
+            >
+              <h2 className="text-xl m-4">Filter</h2>
+              <Filter />
+            </aside>
+          </div>
+          <div className="col-span-4">
+            <div className="text-right">
+              <Sort />
+            </div>
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 p-3 gap-2 w-full">
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
