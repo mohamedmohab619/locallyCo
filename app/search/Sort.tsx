6c06aed70288411e7bfdc1524ae1e24aaf33dc97
Hania Said <haniasaid110@gmail.com>
2025-10-29 07:31:22 +0300

    Replaced flowbite functionality for sort.tsx with useState to solve tree hydration issue

--- app/search/Sort.tsx
+++ app/search/Sort.tsx
@@ -1,19 +1,22 @@
 "use client";
 import React from "react";
-import { initFlowbite } from "flowbite";
+// import { initFlowbite } from "flowbite";
 
 export default function Sort() {
-  React.useEffect(() => {
-    initFlowbite();
-  }, []);
+  // React.useEffect(() => {
+  //   initFlowbite();
+  // }, []);
 
+  const [isOpen, setIsOpen] = React.useState(false);
+  function handleToggle() {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+  }
   return (
     <div>
       <button
-        id="dropdownDefaultButton"
-        data-dropdown-toggle="dropdown"
         className="mx-3 text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-2 focus:outline-none cursor-pointer text-center inline-flex items-center bg-brown hover:bg-dark-brown duration-300"
         type="button"
+        onClick={handleToggle}
       >
         SORT BY
         <svg
@@ -33,36 +36,35 @@ export default function Sort() {
         </svg>
       </button>
 
-      <div
-        id="dropdown"
-        className="z-10 hidden text-left bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44"
-      >
-        <ul
-          className="py-2 text-md text-black"
-          aria-labelledby="dropdownDefaultButton"
-        >
-          <li>
-            <a href="#" className="block px-4 py-2">
-              Recommended
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block px-4 py-2">
-              Newest
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block px-4 py-2">
-              Highest Price
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block px-4 py-2">
-              Lowest Price
-            </a>
-          </li>
-        </ul>
-      </div>
+      {isOpen && (
+        <div className="z-10 hidden text-left bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44">
+          <ul
+            className="py-2 text-md text-black"
+            aria-labelledby="dropdownDefaultButton"
+          >
+            <li>
+              <a href="#" className="block px-4 py-2">
+                Recommended
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block px-4 py-2">
+                Newest
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block px-4 py-2">
+                Highest Price
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block px-4 py-2">
+                Lowest Price
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
